b3a50677758f906569ce5bb095042f7ff843121d
friedpinch <friedpinch@gmail.com>
2017-04-20 10:51:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+getTriangleArea(a, h);
+var a = 2;
+h = 3;
+console.log ('Nieprawidłowe dane');
+} else if (h <= 0) {
+console.log ('Nieprawidłowe dane');
+} else {
+console.log ('funkcja');
+}
+console.log( getTriangleArea(10,6);
+var triangleArea = getTriangleArea(10, 15);
+getTriangleArea()
+
+
